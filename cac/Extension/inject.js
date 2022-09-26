96906d94ae4f727b26f0815580404901b3944700
Username123-coder <73372807+Username123-coder@users.noreply.github.com>
2022-09-25 19:28:57 -0700

    Add files via upload

---
+++ cac/Extension/inject.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    chrome.runtime.sendMessage({method: "getLocalStorage", key: "status"}, function(response) {
+        lists = response.data;
+        console.log(lists);
+        for (let i = 0; i < lists.length; i++) {
+            for (let j = 0; j < lists[i].length; j++) {
+                document.body.innerHTML = document.body.innerHTML.replace(lists[i][j], "[censured]");
+            }
+        }
+    });
+}
