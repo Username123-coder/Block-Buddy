fe0f0cee1f919062a5fae48446f120eca206fcb8
orderlyzero <113956247+orderlyzero@users.noreply.github.com>
2022-10-24 11:37:51 -0700

    Update inject.js

--- cac/Extension/inject.js
+++ cac/Extension/inject.js
@@ -2,9 +2,11 @@ window.onload = () => {
     chrome.runtime.sendMessage({method: "getLocalStorage", key: "status"}, function(response) {
         lists = response.data;
         console.log(lists);
+        //string test = document.body.innerHTML;
+        //this is a test
         for (let i = 0; i < lists.length; i++) {
             for (let j = 0; j < lists[i].length; j++) {
-                document.body.innerHTML = document.body.innerHTML.replaceAll(lists[i][j], "[censured]");
+                document.body.innerHTML = document.body.innerHTML.replaceAll(lists[i][j], "[censored]");
             }
         }
     });
