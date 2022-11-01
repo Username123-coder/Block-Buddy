0181ab26b0328adadd99a7e311ab92c02e077388
plasmatime <plasmaninja12109@gmail.com>
2022-10-31 18:17:01 -0700

    cleaning up comments

--- cac/Extension/inject.js
+++ cac/Extension/inject.js
@@ -10,22 +10,8 @@ chrome.runtime.sendMessage({method: "getLocalStorage", key: "status"}, function(
             if (lists[i][j] != "") {
                 let x = lists[i][j];
                 x.toLowerCase();
-
-    
                 document.body.innerHTML = document.body.innerHTML.replace(new RegExp(x, 'gi'), "[censored]");
-                /*document.body.innerHTML = document.body.innerHTML.replaceAll(y, "[censored]");
-
-                let v = y.indexOf(" ");
-                while (v > -1) {
-                    if (y.charAt(v + 1) == null) {
-                        y = y.substring(0, v + 1) + y.substring(v + 1, v + 2).toUpperCase() + y.substring(v + 2, y.length);
-                    } 
-                    v = y.indexOf(" ", v + 1);
-                }
-                document.body.innerHTML = document.body.innerHTML.replaceAll(y, "[censored]");
-
-                y = y.toUpperCase();
-                document.body.innerHTML = document.body.innerHTML.replaceAll(y, "[censored]");*/
+                
             }
         }
     }
